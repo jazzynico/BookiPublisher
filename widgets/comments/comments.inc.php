a8bcf12e007510fd8112245b9cadd85ae246fefe
Nicolas Dufour <nicoduf@yahoo.fr>
2011-10-10 21:14:36 +0200

    Book page, code clean-up and XHTML1.0 validation.

--- widgets/comments/comments.inc.php
+++ widgets/comments/comments.inc.php
@@ -10,39 +10,38 @@ function comments_render($book, $chapter) {
 <script language="javascript">
 <!--
 function new_freecap() {
-	if(document.getElementById) {
-		// extract image name from image source (i.e. cut off ?randomness)
-		thesrc = document.getElementById("freecap").src;
-		thesrc = thesrc.substring(0,thesrc.lastIndexOf(".")+4);
-		// add ?(random) to prevent browser/isp caching
-		document.getElementById("freecap").src = thesrc+"?"+Math.round(Math.random()*100000);
-	} else {
-		alert("Sorry, cannot autoreload freeCap image\\nSubmit the form and a new freeCap will be loaded");
-	}
+        if(document.getElementById) {
+                // extract image name from image source (i.e. cut off ?randomness)
+                thesrc = document.getElementById("freecap").src;
+                thesrc = thesrc.substring(0,thesrc.lastIndexOf(".")+4);
+                // add ?(random) to prevent browser/isp caching
+                document.getElementById("freecap").src = thesrc+"?"+Math.round(Math.random()*100000);
+        } else {
+                alert("Sorry, cannot autoreload freeCap image\\nSubmit the form and a new freeCap will be loaded");
+        }
 }
 //-->
 </script>
 EOF;
     $html .= '<a name="comments"></a><form action="/widgets/comments/add.php" method="POST">'
-           .'<input type="hidden" name="book" value="'.$book.'">'
-           .'<input type="hidden" name="chapter" value="'.$chapter.'">'
-           .'<table><tr><td>name : </td><td><input type="text" name="name"></td><tr>'
-           .'<td style="vertical-align:text-top";>comment : </td><td><textarea name="comment" cols="40" rows="10"></textarea></td><td></table>'
-           .'<img src="/widgets/comments/freecap/freecap.php" id="freecap"><br>'
-           .'<div class="captcha_help">If you can\'t read the word, <a href="" onClick="this.blur();new_freecap();return false;">click here</a></div>'
-           .'word : <input type="text" name="word"><br>'
-
-           .'<input type="submit" value="Send">'
+           .'<input type="hidden" name="book" value="'.$book.'" />'
+           .'<input type="hidden" name="chapter" value="'.$chapter.'" />'
+           .'<table><tr><td><label for="name">name :</label></td><td><input type="text" id="name" name="name" /></td></tr><tr>'
+           .'<td style="vertical-align: text-top;"><label for="comment">comment :</label></td><td><textarea name="comment" id="comment" cols="40" rows="10"></textarea></td></tr></table>'
+           .'<div><img src="/widgets/comments/freecap/freecap.php" id="freecap" alt="freecap" /></div>'
+           .'<div class="captcha_help">If you can\'t read the word, <a href="" onclick="this.blur();new_freecap();return false;">click here</a></div>'
+           .'<div><label for="word">word :</label><input type="text" id="word" name="word" /><br/>'
+           .'<input type="submit" value="Send" /></div>'
            .'</form>' . "\n";
 
     if (file_exists($comments_file)) {
         $comments = file_get_contents($comments_file);
         $comments = json_decode($comments);
         foreach($comments as $comment) {
-            $comment_html = str_replace("\n", "<br>\n", $comment->comment);
+            $comment_html = str_replace("\n", "<br/>\n", $comment->comment);
             $html .= '<div class="comment">On ' . $comment->date
-                  . ' <span class="name">' . $comment->name 
-                  . '</span> wrote:<br>' 
+                  . ' <span class="name">' . $comment->name
+                  . '</span> wrote:<b/r>'
                   . $comment_html . "</div>\n";
         }
     }
@@ -53,7 +52,7 @@ function comments_widget() {
   return Array(
     "info" => Array(
         "author" => "j",
-		"version" => "1.0"
+                "version" => "1.0"
     )
    );
 }
